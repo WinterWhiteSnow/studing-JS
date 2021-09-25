965e004013727d5c43e8971110c88dbbe6ebcb47
Winter <kso8760@daum.net>
2021-09-25 22:50:01 +0900

    떼껄룩 없는 popcat

--- js.js
+++ js.js
@@ -1,7 +1,10 @@
 const title = document.querySelector("header")
+const body = document.querySelector("body")
+const count = document.querySelector("h4.count")
 
 title.style.color = "white";
 title.style.fontSize = "40px";
+count.style.color = "white";
 const clickNumber = "clickNumber";
 
 let game = 0;
@@ -15,15 +18,32 @@ function saveNumber(){
 }
 
 function clickClick(event) {
-	// location.reload(true);
-	console.dir(event)	
-	console.log(window)
+	const X = event.x;
+	const screenX = window.outerWidth;
+	
+	if (screenX/2 > X) {
+		body.style.backgroundColor = "green";
+		console.log("short!")
+	}
+	else{
+		body.style.backgroundColor = "grey";
+		console.log("long!")
+	}
 	game+=1
 	saveNumber();
 }
 
-const span = document.createElement("span");
-span.innerText = game;
-title.appendChild(span)
+function refresh () {
+	location.reload(true);
+}
+
+function making() {
+	// const span = document.createElement("span");
+	count.innerText = `${game}`;
+	body.style.backgroundColor = "black";
+	// title.appendChild(span)
+	console.log("why");
+}
 
 window.addEventListener("click", clickClick)
+setInterval(making, 1000);
