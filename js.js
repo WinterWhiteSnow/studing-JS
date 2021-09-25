6ec75884f2378c831e28a3b5fec46e1d264e8fc2
Winter <kso8760@daum.net>
2021-09-25 20:16:46 +0900

    pop

--- js.js
+++ js.js
@@ -2,6 +2,26 @@ const title = document.querySelector("header")
 
 title.style.color = "white";
 title.style.fontSize = "40px";
+const clickNumber = "clickNumber";
 
-const wow = "hello world!";
-alert(wow);
+let game = 0;
+const save = parseInt(localStorage.getItem(clickNumber,game));
+if (save) {
+	game = save;
+}
+
+function saveNumber(){
+	localStorage.setItem(clickNumber,game);
+}
+
+function clickClick() {	
+	game+=1
+	saveNumber();
+	location.reload(true);
+}
+
+const span = document.createElement("span");
+span.innerText = game;
+title.appendChild(span)
+
+window.addEventListener("click", clickClick)
