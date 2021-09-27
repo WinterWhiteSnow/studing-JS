78a050a55a1266ac91c04b60b8f3bafeb9c71c08
Winter <kso8760@daum.net>
2021-09-27 19:27:04 +0900

    for연습습습

--- java.js
+++ java.js
@@ -2,12 +2,22 @@ let dance = []
 
 for(var i = 0; i < 10; i++){
 	ob = {
-		id:"id"+i,
-		pw:"pw"+i
+		id:i,
+		pw:i
 	}
    dance.push(ob);
 }
 
 for(var i = 0; i < 10; i++){
-	console.log(dance[i]["pw"])
+	document.write(dance[i]["pw"])
+	console.log(1 == dance[i]["id"],i)
+	if (1 == dance[i]["id"]){
+		console.log("들어가있어요")
+		break
+	}
+	else{
+		console.log("없어요")
+	}
  }
+
+// document.write(dance["id"])
