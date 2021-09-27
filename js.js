8d6221a2c6af5f968b2fe2a3e31e101f2820d701
Winter <kso8760@daum.net>
2021-09-27 17:50:42 +0900

    로그인

--- js.js
+++ js.js
@@ -1,11 +1,21 @@
-const title = document.querySelector("header")
-const body = document.querySelector("body")
-const count = document.querySelector("h4.count")
+const title = document.querySelector("header");
+const body = document.querySelector("body");
+const count = document.querySelector("h4.count");
+const main = document.querySelector("main.login");
+const inputId = document.querySelector(".id");
+const inputPW = document.querySelector(".passward");
+const submitButton = document.querySelector(".submit");
+const form = document.querySelector(".login-form")
 
+
+main.style.marginRight=`${inputId.scrollWidth/2+30}px`;
+submitButton.style.height=`${inputId.offsetHeight+inputPW.offsetHeight}px`;
 title.style.color = "white";
 title.style.fontSize = "40px";
 count.style.color = "white";
 const clickNumber = "clickNumber";
+
+let userList=[]
 
 let game = 0;
 const save = parseInt(localStorage.getItem(clickNumber,game));
@@ -22,10 +32,10 @@ function clickClick(event) {
 	const screenX = window.outerWidth;
 	
 	if (screenX/2 > X) {
-		body.style.backgroundColor = "green";
+		body.style.backgroundColor = "red";
 	}
 	else{
-		body.style.backgroundColor = "grey";
+		body.style.backgroundColor = "blue";
 	}
 	game+=1
 	saveNumber();
@@ -36,12 +46,30 @@ function refresh () {
 }
 
 function making() {
-	// const span = document.createElement("span");
 	count.innerText = `${game}`;
-	body.style.backgroundColor = "black";
+	body.style.backgroundColor = "#A5DE9F";
+	// const span = document.createElement("span");
+	// span.innerText = "wow";
 	// title.appendChild(span)
-	console.log("why");
+	// console.log(window.outerWidth);
 }
 
+function login(event){
+	event.preventDefault();
+	const id = inputId.value;
+	const pw = inputPW.value;
+	ob = {
+		id:id,
+		pw:pw,
+		usercode:Date.now()
+	}
+	inputId.value=""
+	inputPW.value=""
+	userList.push(ob)
+	console.log(userList)
+	
+}
+form.addEventListener("submit",login)
 window.addEventListener("click", clickClick)
-setInterval(making, 500);
+
+setInterval(making, 100);
