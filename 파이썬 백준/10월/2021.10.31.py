01eada565018978a838321ec35be9edda917434d
Winter <kso8760@daum.net>
2021-11-02 16:52:41 +0900

    으라더랑러ㅑㄷ러

--- 파이썬 백준/10월/2021.10.31.py
+++ 파이썬 백준/10월/2021.10.31.py
@@ -1,5 +1,5 @@
 
-# answer = [0,0,1,0,1,5]
+# answer = 0 0 1 0 1 5
 
 the_num = int(input())
 count = 0
@@ -7,7 +7,7 @@ answer_list = []
 while count != the_num:
 	count+=1
 	answer = input().split(" ")
-	answer = [int(i) for i in answer]
+	answer = [float(i) for i in answer]
 	x = answer[0]
 	y = answer[1]
 	r1 = answer[2]
@@ -22,12 +22,20 @@ while count != the_num:
 		else:
 			answer_list.append(0)
 	elif distance_x == 0 or distance_y == 0:
-		if r1+r2 == distance_x or r1+r2 == distance_y:
-			answer_list.append(1) # 참
-		elif r1+r2 > distance_x or r1+r2 > distance_y:
-			answer_list.append(2)
-		else:
-			answer_list.append(0)
+		if distance_x ==0: #x가 0일때
+			if r2-r1 < b < r1+r2:
+				answer_list.append(2)
+			elif r2-r1 == b or r1+r2 == b:
+				answer_list.append(1)
+			else:
+				answer_list.append(0)
+		elif distance_y ==0: #y가 0일때
+			if r2-r1 < a < r1+r2:
+				answer_list.append(2)
+			elif r2-r1 == a or r1+r2 == a:
+				answer_list.append(1)
+			else:
+				answer_list.append(0)
 	else:
 		answer_list.append(2)
 for i in answer_list:
